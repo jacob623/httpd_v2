35da7469040ccaf3ae1abab15e8978edba827831
jacob623 <jacob.a.long@gmail.com>
2017-03-30 16:29:04 -0500

    add collect data

--- files/default/httpd_modules.rb
+++ files/default/httpd_modules.rb
@@ -1,4 +1,10 @@
 
 Ohai.plugin :Apache do
   provides 'apache/modules'
+
+  collect_data :default do
+    apache Mash.new
+    modules_cmd = shell_out('apachectl -t -D DUMP_MODULES')
+    apache[:modules] = modules_cmd.stdout
+  end
 end
