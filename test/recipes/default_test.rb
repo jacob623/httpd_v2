d4b5327357294cd4fadf2cd71c31a912f8008675
jacob623 <jacob.a.long@gmail.com>
2017-03-30 16:36:13 -0500

    building out kitchen tests

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -5,3 +5,9 @@ end
 describe command('curl http://localhost:8080') do
   its(:stdout) { should match(/Welcome admins/) }
 end
+
+plugin_directory = '/tmp/kitchen/ohai/plugins'
+describe command("ohai -d #{plugin_directory} apache") do
+  its(:stdout) { should match(/core_module/) }
+end
+
